3c93042e59bfdea8047592fda0b505dfafa2b015
Chaeyeon Park <chay322p@gmail.com>
2023-09-18 00:16:27 +0900

    19532번 수학은 비대면강의입니다

---
+++ algorithm_java/src/algorithm_java/N19532.java
@@ -0,0 +1,34 @@
+package algorithm_java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class N19532 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st= new StringTokenizer(bf.readLine());
+		StringBuilder sb = new StringBuilder();
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		int e = Integer.parseInt(st.nextToken());
+		int f = Integer.parseInt(st.nextToken());
+		
+        for(int i = -999; i <= 999; i++) { 
+            for(int j = -999; j <= 999; j++) {
+                if(a*i + b*j  == c){ 
+                    if(d*i + e*j  == f) { 
+                        sb.append(i + " " + j + "\n");
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println(sb.toString());
+
+    }
+}
